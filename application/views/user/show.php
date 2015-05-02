a1f0d63aee91eaa87242f430610a8265349a8a69
Trevor Hall <craftelectric@Trevors-MacBook-Pro.local>
2015-05-02 01:41:56 -0700

    post message and comments

--- application/views/user/show.php
+++ application/views/user/show.php
@@ -14,92 +14,78 @@
 </head>
 <body>
 	<?php $this->load->view('partials/welcome_header'); 
-	var_dump($user);
-	var_dump($messages);
-	var_dump($comments);
+	// var_dump($user);
+	// var_dump($messages);
+	// var_dump($comments);
 	?>	
 	<div class="container">
 		<div class='col-md-5'>
-		<table class='table col-md-5'>
-			<thead>
-				<th>Michael Choi</th>
-				<th><a href="">Edit your profile</a></th>
-			</thead>
-			<tr>
-				<td>Registered at:</td>
-				<td>December 24th 2012</td>
-			</tr>
+			<table class='table col-md-5'>
+				<thead>
+					<th><?= $user['first_name']. " ". $user['last_name']  ?></th>
+					<th><a href="/dashboard/edit/<?= $user['id']  ?>">Edit your profile</a></th>
+				</thead>
 				<tr>
-				<td>User ID</td>
-				<td>#1</td>
-			</tr>
+					<td>Registered on:</td>
+					<td><?= $user['created_at']  ?></td>
+				</tr>
 				<tr>
-				<td>Email address</td>
-				<td>michael@village88.com</td>
-			</tr>
+					<td>User ID</td>
+					<td><?= $user['id']  ?></td>
+				</tr>
 				<tr>
-				<td>Description</td>
-				<td>I am happy to be here!</td>
-			</tr>
-		</table>
+					<td>Email address</td>
+					<td><?= $user['email'] ?></td>
+				</tr>
+				<tr> 
+					<td>Description</td>
+					<td><?= $user['description'] ?></td>
+				</tr>
+			</table>
 		</div>
 		<div class='col-md-12 bordered desc'>
-			<form>
+			<form action="/users/post_message" method="post">
 				<h4>Leave a message for Michael</h4>
-				<textarea class="form-control" rows="4"></textarea>
+				<input type="hidden" value="<?= $this->session->userdata('id') ?>" name="author_id"/>
+				<input type="hidden" value="<?= $user['id']  ?>" name="user_id"/>
+				<textarea class="form-control" name="message" rows="4"></textarea>
 				<button type="submit" class="btn btn-success pull-right">Post</button>
 			</form>
 		</div>
-		<div class='col-md-12'>
-			<h5><a href="">Mark Guillen</a> Wrote</h5>
-			<p class='message'>Hi Michael! I am having fun building Boom YEAH!</p>
-		</div>
-		<div class='col-md-11 pull-right'>
-			<h5><a href="">Dianne Manlulu</a> Wrote</h5>
-			<p class='comment'>Awesome!</p>
-		</div>
-		<div class='col-md-11 pull-right'>
-			<form>
-				<textarea class="form-control" rows="4" placeholder="write a message"></textarea>
-				<button type="submit" class="btn btn-success pull-right">Post</button>
-			</form>
-		</div>
-		<div class='col-md-12'>
-			<h5><a href="">John Supsupin</a> Wrote</h5>
-			<p class='message'>Hi Michael! What is the best way to learn to code?</p>
-		</div>
-		<div class='col-md-11 pull-right'>
-			<h5><a href="">KB Tonel</a> Wrote</h5>
-			<p class='comment'>Hi John. Have you checked out village 88?</p>
-		</div>
-		<div class='col-md-11 pull-right'>
-			<h5><a href="">KB Tonel</a> Wrote</h5>
-			<p class='comment'>Hi John. Have you checked out village 88?</p>
-		</div>
-		<div class='col-md-11 pull-right'>
-			<form>
-				<textarea class="form-control" rows="4" placeholder="write a message"></textarea>
-				<button type="submit" class="btn btn-success pull-right">Post</button>
-			</form>
-		</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
+		<?php 
+			foreach($messages as $message)
+			{ 
+			if($message['user_id'] === $user['id'])
+			{ ?>
+				<div class='col-md-12'>
+					<h5><a href="/Users/show/<?= $message['author_id'] ?>"><?= $message['name']  ?></a> wrote</h5>
+					<p class='message'><?= $message['content'] ?></p>
+				</div>
+			<?php 
+				foreach ($comments as $comment)
+				{
+					if($comment['message_id'] === $message['id'])
+					{ ?>
+						<div class='col-md-11 pull-right'>
+							<h5><a href="/Users/show/<?= $comment['user_id'] ?>"><?= $comment['name']  ?></a> wrote</h5>
+							<p class='comment'><?= $comment['content'] ?></p>
+						</div>
+				<?php } ?>
+			<?php } ?>
+				<div class='col-md-11 pull-right'>
+					<form action="/users/post_comment" method="post">
+						<input type="hidden" value="<?= $this->session->userdata('id') ?>" name="user_id"/>
+						<input type="hidden" value="<?= $message['id'] ?>" name="message_id"/>
+						<input type="hidden" value="<?= $user['id']  ?>" name="profile_id"/>
+						<textarea class="form-control" rows="4" placeholder="write a message" name="message"></textarea>
+						<button type="submit" class="btn btn-success pull-right">Post</button>
+					</form>
+				</div>
+			<?php } ?>
+		<?php } ?>
+		
 
 	</div> <!-- end of container -->
-
-
-
 
 </body>
 </html>
